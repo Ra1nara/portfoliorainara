8ec3d558314aa898b2ddcb27eb5d38e56015ebcf
Rainara <rainara294@gmail.com>
2025-07-04 08:54:56 -0300

    scroll suave

--- script.js
+++ script.js
@@ -53,3 +53,17 @@ menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active');
   nav.classList.toggle('active');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
