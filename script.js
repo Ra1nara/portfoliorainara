6bf81dc6d5300aa49e71938fbea0df891b8bd28d
Rainara <rainara294@gmail.com>
2025-11-05 13:04:20 -0300

    adc favicon and adjustments particles-js

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
 particlesJS("particles-js", {
   "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "triangle"
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false
-    },
-    "size": {
-      "value": 3,
-      "random": true
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
+    "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+    "color": { "value": "#ffffff" },
+    "shape": { "type": "star" },
+    "opacity": { "value": 0.7, "random": true },
+    "size": { "value": 3, "random": true },
+    "line_linked": { "enable": true },
     "move": {
       "enable": true,
-      "speed": 4
+      "speed": 2,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false
     }
   },
   "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      }
-    }
+      "detect_on": "window",
+  "events": {
+    "onhover": { "enable": true, "mode": "repulse" },
+    "resize": true
+    },
+    "modes": { "repulse": { "distance": 120, "duration": 0.4 } }
   },
   "retina_detect": true
 });
-  
-
-// Partículas já configuradas (mantive seu código, não mexi)
 
 const menuBtn = document.getElementById('menu-btn');
 const nav = document.getElementById('nav');
